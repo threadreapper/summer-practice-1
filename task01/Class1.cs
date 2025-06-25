75aa54d108a9d1dc338eabf255dbff87bc1a819f
timothy <caveiroussight@gmail.com>
2025-06-25 19:46:51 +0600

    palindrom realization

--- task01/Class1.cs
+++ task01/Class1.cs
@@ -1,6 +1,15 @@
-﻿namespace task01;
-
-public class Class1
+﻿using System;
+namespace task01
 {
+    public static class StringExtensions
+    {
+        public static bool IsPalindrome(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return false;
 
+            string formatted = new([.. input.Where(c => !char.IsPunctuation(c) && !char.IsWhiteSpace(c)).Select(char.ToLower)]);
+            return formatted.SequenceEqual(formatted.Reverse());
+        }
+    }
 }
