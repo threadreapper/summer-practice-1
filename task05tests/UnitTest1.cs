fa6549dd662df2bb2e7cd04e99b149c7eb6489f9
timothy <caveiroussight@gmail.com>
2025-06-27 23:16:25 +0600

    classAnalyzer and tests are done

--- task05tests/UnitTest1.cs
+++ task05tests/UnitTest1.cs
@@ -1,10 +1,61 @@
 ﻿namespace task05tests;
 
-public class UnitTest1
+using Xunit;
+using task05;
+
+public class TestClass
+{
+    public int PublicField;
+    private string _privateField;
+    public int Property { get; set; }
+    static int SwagProperty { get; set; }
+
+    public void Method(int woow) { }
+}
+
+[Serializable]
+public class AttributedClass { }
+
+public class ClassAnalyzerTests
 {
     [Fact]
-    public void Test1()
+    public void GetPublicMethods_ReturnsCorrectMethods()
     {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var methods = analyzer.GetPublicMethods();
 
+        Assert.Contains("Method", methods);
+    }
+
+    [Fact]
+    public void GetAllFields_IncludesPrivateFields()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var fields = analyzer.GetAllFields();
+
+        Assert.Contains("_privateField", fields);
+    }
+
+    [Fact]
+    public void GetMethodParams_ReturnsParams()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var param = analyzer.GetMethodParams("Method");
+
+        Assert.Contains("woow", param);
+    }
+
+    [Fact]
+    public void HasAttribute_WorksWithMethodAttributes()
+    {
+        var analyzer = new ClassAnalyzer(typeof(AttributedClass));
+        Assert.True(analyzer.HasAttribute<SerializableAttribute>());
+    }
+
+    [Fact]
+    public void GetProperties_ReturnsCorrectProperties()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        Assert.Contains("SwagProperty", analyzer.GetProperties());
     }
 }
