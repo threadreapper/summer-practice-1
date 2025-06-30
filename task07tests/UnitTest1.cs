e4779124fbca467f9409de5268aba904932e58fa
timothy <caveiroussight@gmail.com>
2025-06-30 18:12:22 +0600

    attributes realization

--- task07tests/UnitTest1.cs
+++ task07tests/UnitTest1.cs
@@ -1,10 +1,47 @@
 ﻿namespace task07tests;
 
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
+using Xunit;
+using task07;
+using System;
+using System.Reflection;
 
+public class AttributeReflectionTests
+{
+    
+    [Fact]
+    public void Class_HasDisplayNameAttribute()
+    {
+        var type = typeof(SampleClass);
+        var attribute = type.GetCustomAttribute<DisplayNameAttribute>();
+        Assert.NotNull(attribute);
+        Assert.Equal("тестовое имя класса какое-то", attribute.DisplayName);
+    }
+
+    [Fact]
+    public void Method_HasDisplayNameAttribute()
+    {
+        var method = typeof(SampleClass).GetMethod("TestMethod");
+        var attribute = method.GetCustomAttribute<DisplayNameAttribute>();
+        Assert.NotNull(attribute);
+        Assert.Equal("какой-то там метод", attribute.DisplayName);
+    }
+
+    [Fact]
+    public void Property_HasDisplayNameAttribute()
+    {
+        var prop = typeof(SampleClass).GetProperty("Number");
+        var attribute = prop.GetCustomAttribute<DisplayNameAttribute>();
+        Assert.NotNull(attribute);
+        Assert.Equal("числосвойство какое-то", attribute.DisplayName);
+    }
+
+    [Fact]
+    public void Class_HasVersionAttribute()
+    {
+        var type = typeof(SampleClass);
+        var attribute = type.GetCustomAttribute<VersionAttribute>();
+        Assert.NotNull(attribute);
+        Assert.Equal(1, attribute.Major);
+        Assert.Equal(0, attribute.Minor);
     }
 }
