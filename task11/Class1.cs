d26a76bca1e05736304f10c67feb09945739ea37
timothy <caveiroussight@gmail.com>
2025-07-09 12:05:33 +0600

    midworking realization

--- task11/Class1.cs
+++ task11/Class1.cs
@@ -1,6 +1,52 @@
 ﻿namespace task11;
 
-public class Class1
-{
+using System;
+using System.Reflection;
+using System.Reflection.Emit;
 
+public static class CalculatorGenerator
+{
+    public static dynamic GenerateCalc()
+    {
+        AssemblyName assemblyName = new("CalcAss");
+        AssemblyBuilder assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Run);
+        ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule("DynamicCalcModule");
+        TypeBuilder typeBuilder = moduleBuilder.DefineType("DynamicCalc.Calculator", TypeAttributes.Public | TypeAttributes.Class);
+
+        GenerateMethod(typeBuilder, "Add", typeof(int), typeof(int), typeof(int));
+        GenerateMethod(typeBuilder, "Minus", typeof(int), typeof(int), typeof(int));
+        GenerateMethod(typeBuilder, "Mul", typeof(int), typeof(int), typeof(int));
+        GenerateMethod(typeBuilder, "Div", typeof(int), typeof(int), typeof(int));
+        
+        Type calculatorType = typeBuilder.CreateType();
+
+        return Activator.CreateInstance(calculatorType);
+    }
+
+    private static void GenerateMethod(TypeBuilder typeBuilder, string methodName, Type returnType, params Type[] parameterTypes)
+    {
+        MethodBuilder methodBuilder = typeBuilder.DefineMethod(methodName, MethodAttributes.Public, returnType, parameterTypes);
+        ILGenerator il = methodBuilder.GetILGenerator();
+
+        il.Emit(OpCodes.Ldarg_1);
+        il.Emit(OpCodes.Ldarg_2);
+
+        switch (methodName)
+        {
+            case "Add":
+                il.Emit(OpCodes.Add);
+                break;
+            case "Minus":
+                il.Emit(OpCodes.Sub);
+                break;
+            case "Mul":
+                il.Emit(OpCodes.Mul);
+                break;
+            case "Div":
+                il.Emit(OpCodes.Div);
+                break;
+        }
+
+        il.Emit(OpCodes.Ret);
+    }
 }
--- task11tests/UnitTest1.cs
+++ task11tests/UnitTest1.cs
@@ -1,10 +1,51 @@
-﻿namespace task11tests;
+﻿using Xunit;
+using task11;
 
-public class UnitTest1
+namespace task11tests
 {
-    [Fact]
-    public void Test1()
+    public class CalculatorTests
     {
+        [Fact]
+        public void TestAdd_ReturnsCorrectAnsw()
+        {
+            dynamic calculator = CalculatorGenerator.GenerateCalc();
+            int result = calculator.Add(5, 3);
+            Assert.Equal(8, result);
+        }
 
+        [Fact]
+        public void TestMinus_ReturnsCorrectAnsw()
+        {
+            dynamic calculator = CalculatorGenerator.GenerateCalc();
+            int result = calculator.Minus(5, 3);
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void TestMul_ReturnsCorrectAnsw()
+        {
+            dynamic calculator = CalculatorGenerator.GenerateCalc();
+            int result = calculator.Mul(5, 3);
+            Assert.Equal(15, result);
+        }
+
+        [Fact]
+        public void TestDiv_ReturnsCorrectAnsw()
+        {
+            dynamic calculator = CalculatorGenerator.GenerateCalc();
+            int result = calculator.Div(6, 3);
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void TestAllOperationa()
+        {
+            dynamic calculator = CalculatorGenerator.GenerateCalc();
+
+            Assert.Equal(10, calculator.Add(7, 3));
+            Assert.Equal(4, calculator.Minus(7, 3));
+            Assert.Equal(21, calculator.Mul(7, 3));
+            Assert.Equal(2, calculator.Div(7, 3));
+        }
     }
 }
