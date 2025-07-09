04c2826646ec3751e7f73e6dd4a234de2239887a
timothy <caveiroussight@gmail.com>
2025-07-09 12:23:07 +0600

    exercise fix (check the 8th line)

--- task13/Class1.cs
+++ task13/Class1.cs
@@ -1,4 +1,23 @@
-﻿namespace task13;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+
+namespace task13;
+
+public class Subject //добавлил public, так как иначе в 19 строке ошибка "Inconsistent accessibility: property type 'List<Subject>' is less accessible than property 'Student.Grades'"
+{
+    public string Name { get; set; }
+    public int Grade { get; set; }
+}
+
+public class Student
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime BirthDate { get; set; }
+    public List<Subject> Grades { get; set; }
+}
 
 public class Class1
 {
