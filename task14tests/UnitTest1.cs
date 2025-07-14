6b55ab34f2b11058803550cb1f8d8cf365678fca
timothy <caveiroussight@gmail.com>
2025-07-14 20:36:37 +0600

    thread integrals realization

--- task14tests/UnitTest1.cs
+++ task14tests/UnitTest1.cs
@@ -1,10 +1,34 @@
-﻿namespace task14tests;
+﻿using task14;
+namespace task14tests;
 
+using Xunit;
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
-    {
+    Func<double, double> X = x => x;
+    Func<double, double> SIN = x => Math.Sin(x);
 
+    [Fact]
+    public void Solve_LinearFunction()
+    {
+        Assert.Equal(0, DefiniteIntegral.Solve(-1, 1, X, 1e-4, 2), 1e-4);
+    }
+
+    [Fact]
+    public void Solve_AnotherLinearFunction()
+    {
+        Assert.Equal(0, DefiniteIntegral.Solve(-1, 1, X, 1e-4, 2), 1e-4);
+    }
+
+    [Fact]
+    public void Solve_SinFunction()
+    {
+        Assert.Equal(0, DefiniteIntegral.Solve(-1, 1, SIN, 1e-5, 8), 1e-4);
+    }
+
+    [Fact]
+    public void Solve_InvalidThreadException()
+    {
+        Assert.Throws<Exception>(() => DefiniteIntegral.Solve(0, 1, X, 1e-4, 0));
+        Assert.Throws<Exception>(() => DefiniteIntegral.Solve(0, 1, X, 1e-4, -1));
     }
 }
